622c9a6aeb4f77f54f2e7b6c3c11ea2bea243a5e
Sergey Aksenov <asm96@users.noreply.github.com>
2021-08-06 08:01:01 +0300

    fixed js

--- fullscreen-menu.js
+++ fullscreen-menu.js
@@ -1,6 +1,7 @@
 var hamburger = document.querySelector(".hamburger");
 var menu = document.querySelector(".fullscreen-menu");
 var closeBtn = menu.querySelector(".fullscreen-menu__close");
+var menuItems = document.querySelectorAll(".menu__item");
 hamburger.addEventListener("click", function (e) {
   e.preventDefault(),
     menu.classList.add("active")
@@ -9,3 +10,9 @@ closeBtn.addEventListener("click", function (e) {
   e.preventDefault(),
     menu.classList.remove("active")
 });
+for (var i = 0; i < menuItems.length; i++) {
+  menuItems[i].onclick = function(e){
+    e.preventDefault(),
+    menu.classList.remove("active")
+  };
+}
