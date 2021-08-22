69f15db689f71fe75e2704f352bf5d020bd10f10
Sergey Aksenov <asm96@users.noreply.github.com>
2021-08-22 20:03:10 +0300

    final

---
+++ gulp.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  SRC_PATH: "src",
+  DIST_PATH: "docs",
+  STYLES_LIBS: ["node_modules/normalize.css/normalize.css"],
+  JS_LIBS: [
+    "node_modules/jquery/dist/jquery.js",
+    "node_modules/jquery-touchswipe/jquery.touchSwipe.js",
+    "node_modules/mobile-detect/mobile-detect.js",
+  ],
+};
---
+++ src/scripts/slider.js
@@ -0,0 +1,15 @@
+(function () {
+  const slider = $(".products").bxSlider({
+    pager: false,
+    controls: false,
+  });
+
+  $(".product-slider__arrow_direction_prev").click((e) => {
+    e.preventDefault();
+    slider.goToPrevSlide();
+  });
+  $(".product-slider__arrow_direction_next").click((e) => {
+    e.preventDefault();
+    slider.goToNextSlide();
+  });
+})();
